14d093739243706912afea8122d27c584abb5aa4
Guilherme Leoi <geeleonidas@pm.me>
2023-02-27 17:23:03 -0300

    Add filter to newer messages

--- src/carlito/core.nim
+++ src/carlito/core.nim
@@ -87,6 +87,7 @@ proc pickContent*(s: Shard, channelId: string): Future[string] {.async.} =
          m.mentionsUser(s.user) or
          m.author.id == s.user.id or
          m.content.hasUnsafeDomains() or
+         selectedDateTime.toTime() > timestamp(m.id) or 
          m.content.match(re"(p|P)(e|E)(t|T)(i|I)(t|T)"):
         continue
       return m.content
